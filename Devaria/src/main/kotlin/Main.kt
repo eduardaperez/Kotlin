8b02f185919d8926520c38547d1ef3d173169364
eduardaperez <c.eduardaperez@gmail.com>
2024-09-09 21:14:42 -0300

    Aula 1 Devaria

--- Devaria/src/main/kotlin/Main.kt
+++
@@ -1,4 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello World! This is my first Kotlin project")
-
-}
--- Devaria/src/main/kotlin/classes/Produto.kt
+++
@@ -1,7 +0,0 @@
-package classes
-
-class Produto (val nome : String, val preco : Double) {
-    fun exibirDadosProduto () : String {
-        return "Produto nome: $nome e valor: R$ $preco"
-    }
-}
